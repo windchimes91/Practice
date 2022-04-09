d01758ba7453a7a28aa665dcfdab7a136612d3a2
unknown <C110156152@nkust.edu.tw>
2022-04-10 00:18:29 +0800

    four commit

--- 18 撲克牌13點.py
+++ 18 撲克牌13點.py
@@ -1,14 +1,14 @@
-s = input("輸入五張牌：")
+s = input("輸入五張牌（以空白相隔）：")
 sum = 0
 list1 = s.split()
 for i in range(len(list1)):
-    if list1[i] == "A":
+    if list1[i] == "A" or list1[i] == "a":
         sum += 1
-    elif list1[i] == "J":
+    elif list1[i] == "J" or list1[i] == "j":
         sum += 11
-    elif list1[i] == "Q":
+    elif list1[i] == "Q" or list1[i] == "q":
         sum += 12
-    elif list1[i] == "K":
+    elif list1[i] == "K" or list1[i] == "k":
         sum += 13
     else:
         sum += int(list1[i])
